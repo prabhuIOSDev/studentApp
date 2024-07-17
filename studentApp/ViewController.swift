c46fa7517b3c40e9c56b64d765d13f4cb16b28c7
prabhuIOSDev <prabhuiosdeveloper@gmail.com>
2024-07-17 12:24:36 +0530

    Initial Commit

---
+++ studentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  studentApp
+//
+//  Created by Prabhu on 17/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
